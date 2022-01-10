7d7249ffd6f687af910995b059125bc0bdb98a29
aynulislam <itamxeed7@gmail.com>
2022-01-11 02:07:26 +0600

    test2 file added

---
+++ test2.py
@@ -0,0 +1,28 @@
+
+
+
+bangla_marks = 75
+english_marks = 88
+physics_marks = 60
+
+total_marks = (bangla_marks + english_marks + physics_marks)
+
+avg_marks = (total_marks / 3)
+c = avg_marks
+
+#condtional Logic Implement
+
+
+my_marks = int(input('Enter Your Marks:'))
+
+
+if my_marks>=70 and my_marks<=79:
+    print('A')
+elif my_marks<70 and my_marks>=60:
+    print('A-')
+else:
+    print('nothing')
+
+
+
+
